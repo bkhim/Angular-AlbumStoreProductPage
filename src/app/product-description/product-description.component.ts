5fe5eb0b244a069903a99356e2c3434995d528b0
Blancos Khim <briansiranga@gmail.com>
2019-10-24 11:33:27 +0300

    3 check

--- src/app/product-description/product-description.component.ts
+++ src/app/product-description/product-description.component.ts
@@ -11,10 +11,12 @@ import { ProductService } from '../product.service';
 })
 export class ProductDescriptionComponent implements OnInit {
 
+  albumInfo;
+
   constructor(private _productService) { }
 
   ngOnInit() {
-    this._productService.getAlbum().subscribe()
+    this._productService.getAlbum(1).subscribe(response => this.albumInfo = response)
   }
 
 }
